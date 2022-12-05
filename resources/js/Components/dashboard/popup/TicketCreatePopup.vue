a55e16fd74fdffc521e4cc64f7b20deb13ec824f
Muhibbullah Ansary <muhibbullah611@gmail.com>
2022-12-06 02:47:19 +0600

    working event create

--- resources/js/Components/dashboard/popup/TicketCreatePopup.vue
+++ resources/js/Components/dashboard/popup/TicketCreatePopup.vue
@@ -32,17 +32,19 @@
                  <label for="name">Stock</label>
                  <div class="two-columns">
                     <select v-model="formData.ticket_stock" id="tickets_stock">
+                       <option value="0">-select-</option>
                        <option value="limited">Limited Stock</option>
                        <option value="unlimited">Unlimited Stock</option>
                     </select>
-                    <input v-if="(formData.ticket_stock == 'limited')" v-model="formData.stock_limit" type="number" id="tickets_stock_input">
+                    <input v-if="(formData.ticket_stock == 'limited')" min="1" step="1" v-model="formData.stock_limit" type="number" id="tickets_stock_input">
                  </div>
               </div>
-              <div v-if="formData.ticketType == 'paid'" class="element pricing" id="ticket_price">
+
+              <div v-if="formData.ticketType == 'Paid'" class="element pricing" id="ticket_price">
                  <label for="paid">Price</label>
                  <div class="two-columns">
                     <input type="text" value="$" disabled="">
-                    <input v-model="formData.price" type="number">
+                    <input v-model="formData.price" min="0" type="number">
                  </div>
               </div>
 
@@ -61,35 +63,49 @@
                  <textarea v-model="formData.ticket_description" name="Ticket description"></textarea>
               </div>
               <div class="element perks" id="perks">
-                 <input v-model="formData.perks" type="text" placeholder="Perks for this ticket" id="perks_input"> 
-                 <div class="add-perk" id="add_perk">
-                    + Add
-                </div>
+                  <input v-model="formData.perks" type="text" placeholder="Perks for this ticket" id="perks_input"> 
+                  <div class="add-perk" id="add_perk">
+                     + Add
+                  </div>
               </div>
               <div class="element- mb-6">
                  <div class="two-columns">
-                    <input type="checkbox" >
-                    <label for="perk checkbox">Transfer fees to guest</label>
+                    <input type="checkbox" v-model="formData.isTransferFeesToGuest" >
+                    <label>Transfer fees to guest</label>
                  </div>
               </div>
               <h6 class="additional-information-heading">Do you want to collect additional information?</h6>
-              <div class="element">
-                 <label for="question">Custom question</label>
-                 <input type="text" name="question"> 
-              </div>
-              <div class="element-">
-                 <div class="two-columns">
-                    <input type="checkbox" name="perk checkbox">
-                    <label for="perk checkbox">Make this question required</label>
-                 </div>
-              </div>
-              <div class="add-other-question">
-                 <a href="#">Add an other question</a> 
+              
+               <template v-for="(item, index) in questions" :key="index">
+                  <div class="element">
+                     <label for="question">Custom question</label>
+                     <input type="text" v-model="item.question" name="question"> 
+                  </div>
+                  <div class="element-">
+                     <div class="two-columns relative">
+                        <button
+                           v-if="(index != 0)" 
+                           class="text-sm absolute right-0 bottom-4 text-red-500"
+                           @click="() => {
+                              questions.splice(index, 1)
+                           }"
+                        >
+                           Remove
+                        </button>
+                        <input type="checkbox" :checked="item.isRequired">
+                        <label for="perk checkbox">Make this question required</label>
+                     </div>
+                  </div>
+               </template>
+
+              <div class="add-other-question mt-8">
+                 <button @click="questions.push(placeholderQuestion)">Add an other question</button> 
                  <img src="../../../assets/images/star-bars.svg" alt="move-pro icon">
               </div>
+
               <div class="buttons">
                  <div class="button btn-light">skip</div>
-                 <div class="button" @click="test">continue</div>
+                 <div class="button" @click="nextStep">continue</div>
               </div>
            </div>
         </div>
@@ -97,21 +113,34 @@
 </template>
 
 <script setup>
+   import { ref } from 'vue'
    import useEvent from '@/Pages/useEvent';
-   const { formData, ticketTypes } = useEvent()
-    const props = defineProps({
-        showPopup: {
-            default: 'Test'
-        },
-        modelValue: {
-            type: Boolean
-        }
-    })
 
-   const test = () => {
+   const placeholderQuestion = {
+         question: '',
+         isRequired: false
+   }
+   const questions = ref([
+      { ...placeholderQuestion }
+   ])
+
+   const { formData, ticketTypes } = useEvent()
+   const props = defineProps({
+      modelValue: {
+         type: Boolean
+      }
+   })
+
+   const nextStep = () => {
+      formData.value.question = questions
       console.log(formData.value)
    }
 </script>
 
 <style scoped>
+.screen-overlay .create-ticket .content{
+   width: 100%;
+   height: 94%;
+   overflow-y: auto;
+}
 </style>
