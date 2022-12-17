73e915162a192886824248105a2aaab4a898ecad
Nasrullah(nasir) <nasrullah23a@gmail.com>
2022-12-17 10:07:56 +0600

    note

--- routes/my-route/advertise.php
+++ routes/my-route/advertise.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\AvaertiseController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('event/advertise/', [AvaertiseController::class, 'index'])->name('advertise.index');
-Route::post('event/advertise/store', [AvaertiseController::class, 'store'])->name('advertise.store');
-Route::delete('event/advertise/delete', [AvaertiseController::class, 'delete'])->name('advertise.delete');
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('event/advertise/', [AvaertiseController::class, 'index'])->name('advertise.index');
+    Route::post('event/advertise/store', [AvaertiseController::class, 'store'])->name('advertise.store');
+    Route::delete('event/advertise/delete', [AvaertiseController::class, 'delete'])->name('advertise.delete');
+});
